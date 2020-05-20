9080eca13c45cb074cd5f20e09d368226b98ce86
Matt Wilber <matt@greenzeta.com>
2020-05-20 16:52:34 -0700

    Addresses #36 - Add custom post types (Update, Project, Portfolio) -

--- functions.php
+++ functions.php
@@ -190,6 +190,8 @@ flush_rewrite_rules( false );
 // Register Custom Post Type
 function update_post_type() {
 
+    $taxs = array( 'category', 'post_tag' );
+    
 	$labels = array(
 		'name'                  => 'Updates',
 		'singular_name'         => 'Update',
@@ -224,7 +226,7 @@ function update_post_type() {
 		'description'           => 'Personal Project Updates',
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'excerpt' ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
+		'taxonomies'            => $taxs,
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
@@ -238,62 +240,82 @@ function update_post_type() {
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
 	);
-	register_post_type( 'update', $args );
+    register_post_type( 'update', $args );
+
+    $args['labels']['name'] = 'Projects';
+    $args['labels']['singular_name'] = 'Project';
+    $args['labels']['menu_name'] = 'Projects';
+    $args['labels']['name_admin_bar'] = 'Project';
+    $args['label'] = 'Project';
+    $args['description'] = 'GZ Originals';
+    $args['taxonomies'] = $taxs;
+
+    register_post_type( 'project', $args );
+
+    $args['labels']['name'] = 'Portfolio';
+    $args['labels']['singular_name'] = 'Portfolio';
+    $args['labels']['menu_name'] = 'Portfolio';
+    $args['labels']['name_admin_bar'] = 'Portfolio';
+    $args['label'] = 'Portfolio';
+    $args['description'] = 'Professional Work';
+    $args['taxonomies'] = array( 'post_tag' );
+
+    register_post_type( 'portfolio', $args );
 
 }
 add_action( 'init', 'update_post_type', 0 );
 
-// Register Custom Post Type
-function portfolio_post_type() {
+// // Register Custom Post Type
+// function portfolio_post_type() {
 
-	$labels = array(
-		'name'                  => _x( 'Portfolio', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Portfolio', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Portfolio', 'text_domain' ),
-		'name_admin_bar'        => __( 'Portfolio', 'text_domain' ),
-		'archives'              => __( 'Item Archives', 'text_domain' ),
-		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-		'all_items'             => __( 'All Items', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Item', 'text_domain' ),
-		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-		'update_item'           => __( 'Update Item', 'text_domain' ),
-		'view_item'             => __( 'View Item', 'text_domain' ),
-		'view_items'            => __( 'View Items', 'text_domain' ),
-		'search_items'          => __( 'Search Item', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
-	);
-	$args = array(
-		'label'                 => __( 'Portfolio', 'text_domain' ),
-		'description'           => __( 'Green Zeta Portfolio Project', 'text_domain' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'excerpt' ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-	);
-	register_post_type( 'portfolio', $args );
+// 	$labels = array(
+// 		'name'                  => _x( 'Portfolio', 'Post Type General Name', 'text_domain' ),
+// 		'singular_name'         => _x( 'Portfolio', 'Post Type Singular Name', 'text_domain' ),
+// 		'menu_name'             => __( 'Portfolio', 'text_domain' ),
+// 		'name_admin_bar'        => __( 'Portfolio', 'text_domain' ),
+// 		'archives'              => __( 'Item Archives', 'text_domain' ),
+// 		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+// 		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+// 		'all_items'             => __( 'All Items', 'text_domain' ),
+// 		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+// 		'add_new'               => __( 'Add New', 'text_domain' ),
+// 		'new_item'              => __( 'New Item', 'text_domain' ),
+// 		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+// 		'update_item'           => __( 'Update Item', 'text_domain' ),
+// 		'view_item'             => __( 'View Item', 'text_domain' ),
+// 		'view_items'            => __( 'View Items', 'text_domain' ),
+// 		'search_items'          => __( 'Search Item', 'text_domain' ),
+// 		'not_found'             => __( 'Not found', 'text_domain' ),
+// 		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+// 		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+// 		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+// 		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+// 		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+// 		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+// 		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+// 		'items_list'            => __( 'Items list', 'text_domain' ),
+// 		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+// 		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+// 	);
+// 	$args = array(
+// 		'label'                 => __( 'Portfolio', 'text_domain' ),
+// 		'description'           => __( 'Green Zeta Portfolio Project', 'text_domain' ),
+// 		'labels'                => $labels,
+// 		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'excerpt' ),
+// 		'taxonomies'            => array( 'category', 'post_tag' ),
+// 		'hierarchical'          => false,
+// 		'public'                => true,
+// 		'show_ui'               => true,
+// 		'show_in_menu'          => true,
+// 		'menu_position'         => 5,
+// 		'show_in_admin_bar'     => true,
+// 		'show_in_nav_menus'     => true,
+// 		'can_export'            => true,
+// 		'has_archive'           => true,
+// 		'exclude_from_search'   => false,
+// 		'publicly_queryable'    => true,
+// 	);
+// 	register_post_type( 'portfolio', $args );
 
-}
-add_action( 'init', 'portfolio_post_type', 0 );
+// }
+// add_action( 'init', 'portfolio_post_type', 0 );
